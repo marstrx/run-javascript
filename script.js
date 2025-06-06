8faf3b44b6ed17651bdc026816d926dc3a4b02a8
Marouane Bachar <marstrone@gmail.com>
2025-06-06 17:55:04 +0200

    almost evrything is working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const codeInput = document.getElementById('codeInput');
 const runButton = document.getElementById('runBtn');
 const outputContent = document.getElementById('outputContent');
+const clearBtn = document.getElementById('clearBtn');
 
 runButton.addEventListener('click', () => {
     const content = codeInput.value;
@@ -10,4 +11,9 @@ runButton.addEventListener('click', () => {
     } catch (e) {
         outputContent.srcdoc = `<p style="color:red;">Error: ${e.message}</p>`;
     }
+});
+
+clearBtn.addEventListener('click', () => {
+    codeInput.value = '';
+    outputContent.srcdoc = '';
 });
